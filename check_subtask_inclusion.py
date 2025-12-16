d94c0008fe95101c60a19a900c36c1c33b300642
Matistjati <matistjati@outlook.com>
2025-12-16 11:00:23 +0100

    Set unlimited stack for recursive input validators

--- check_subtask_inclusion.py
+++ check_subtask_inclusion.py
@@ -14,6 +14,7 @@ from typing import Iterable, List, Any
 import argparse
 
 resource.setrlimit(resource.RLIMIT_AS, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
+resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
